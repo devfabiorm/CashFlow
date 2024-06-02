83739bad014acc1352d50c8316aa9fd617b12315
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-01 23:33:09 -0300

    Initializing the class responsible for handle business rules to register a expense

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpanseUseCase.cs
@@ -0,0 +1,12 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpanseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        //ToDo Validations
+        return new ResponseRegisteredExpenseJson();
+    }
+}
