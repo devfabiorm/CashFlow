aaee0531ba352545d290a09523426020fde24de8
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-11 22:18:22 -0300

    Passing configuration dynamically through dependency injection container

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -4,13 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace CashFlow.Infrastructure.DataAccess;
 internal class CashFlowDbContext : DbContext
 {
-    public DbSet<Expense> Expenses { get; set; }
+    public CashFlowDbContext(DbContextOptions options) : base(options) { } 
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=root;";
-        var version = new Version(8, 0, 37);
-        var serverVersion = new MySqlServerVersion(version);
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
+    public DbSet<Expense> Expenses { get; set; }
 }
