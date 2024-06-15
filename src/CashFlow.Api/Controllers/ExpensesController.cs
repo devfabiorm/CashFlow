8a9153c8357f0812fb7bd567a21ad0ef9921786a
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-14 23:59:12 -0300

    Created method to retrieve an expense by id

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
@@ -31,5 +32,18 @@ public class ExpensesController : ControllerBase
             return Ok(response);
 
         return NoContent();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(ResponseExpenseJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetById(
+        [FromServices] IGetExpenseByIdUseCase usecase, 
+        [FromRoute] long id)
+    {
+        var response = await usecase.Execute(id);
+
+        return Ok(response);
     }
 }
