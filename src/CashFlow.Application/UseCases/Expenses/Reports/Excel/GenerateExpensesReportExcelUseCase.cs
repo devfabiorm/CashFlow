c7d3cead3f4a6cf64e274b31518de88452044921
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-18 23:20:52 -0300

    Installing and starting work with ClosedXML

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,17 @@
+﻿
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+    public Task<byte[]> Execute(DateOnly month)
+    {
+        var workbook = new XLWorkbook();
+
+        workbook.Author = "Fabio Ribeiro";
+        workbook.Style.Font.FontSize = 12;
+        workbook.Style.Font.FontName = "Times New Roman";
+
+        var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+    }
+}
