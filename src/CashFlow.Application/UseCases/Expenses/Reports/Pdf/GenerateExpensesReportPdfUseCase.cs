e95613ad8c07dd3358f7487c97625d309760807c
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-22 18:57:58 -0300

    Created service structure to generate a pdf report

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
+{
+    private const string CURRENT_CURRENT = "€";
+    private readonly IExpensesReadOnlyRepository _repository;
+
+    public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _repository.FilterByMonth(month);
+
+        if (expenses.Count == 0)
+        {
+            return [];
+        }
+
+        return [];
+    }
+}
