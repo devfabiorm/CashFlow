372ff6896c9a03b988d04b8a04ce84fea1df928e
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-22 22:08:41 -0300

    Implemented font resolver

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+public class FontHelper
+{
+    public const string RALEWAY_RELAGULAR = "Raleway-Regular";
+    public const string RALEWAY_BLACK = "Raleway-Black";
+    public const string WORKSANS_RELAGULAR = "WorkSans-Regular";
+    public const string WORKSANS_BLACK = "WorkSans-Black";
+
+    public const string DAFAULT_FONT = RALEWAY_RELAGULAR;
+}
+   
