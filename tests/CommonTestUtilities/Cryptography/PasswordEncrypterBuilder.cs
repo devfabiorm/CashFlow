da2c4568d7d9a96f8b7798ff6d43f88bddf420bb
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-08-14 23:00:41 -0300

    wip: created classes to handler building mocks and the beginning of success test for the use case of user registration

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncrypterBuilder.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Domain.Security.Cryptography;
+using Moq;
+
+namespace CommonTestUtilities.Cryptography;
+public class PasswordEncrypterBuilder
+{
+    public static IPasswordEncrypter Build()
+    {
+        var mock = new Mock<IPasswordEncrypter>();
+
+        mock.Setup(passwordEncryptyer => passwordEncryptyer.Encrypt(It.IsAny<string>()))
+            .Returns("!a$%*adj@!d23");
+
+        return mock.Object;
+    }
+}
---
+++ tests/UseCases.Testes/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Token;
+using FluentAssertions;
+
+namespace UseCases.Testes.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    public async Task Success()
+    {
+        //Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        //Act
+        var result = await useCase.Execute(request);
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Name.Should().Be(request.Name);
+        result.Token.Should().NotBeNullOrWhiteSpace();
+    } 
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        var mapper = MapperBuilder.Build();
+        var userWriteOnlyRepository = UserWriteOnlyRepositoryBuilder.Build();
+        var unitOFWork = UnitOfWorkBuilder.Build();
+        var passwordEncrypter = PasswordEncrypterBuilder.Build();
+        var jwtTokenGenerator = JwtTokenGeneratorBuilder.Build();
+
+        return new RegisterUserUseCase(mapper, passwordEncrypter, null, userWriteOnlyRepository, unitOFWork, jwtTokenGenerator);
+    }
+}
