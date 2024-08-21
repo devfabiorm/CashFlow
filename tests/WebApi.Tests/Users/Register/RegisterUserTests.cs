29bb9c15dcbf8d0955462c2e93373152c87d5461
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-08-20 21:56:17 -0300

    feat: integration test for user registration

--- tests/WebApi.Tests/Users/Register/RegisterUserTests.cs
+++ tests/WebApi.Tests/Users/Register/RegisterUserTests.cs
@@ -2,11 +2,12 @@
 using FluentAssertions;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace WebApi.Tests.Users.Register;
 public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
 {
-    private const string METHOD = "api/User";
+    private const string METHOD = "api/Users";
 
     private readonly HttpClient _httpClient;
 
@@ -19,12 +20,19 @@ public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
     public async Task Success()
     {
         //Arrange
-        var request = RequestRegisterExpenseJsonBuilder.Build();
+        var request = RequestRegisterUserJsonBuilder.Build();
 
         //Act
         var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
         //Assert
         result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
+        response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
     }
 }
