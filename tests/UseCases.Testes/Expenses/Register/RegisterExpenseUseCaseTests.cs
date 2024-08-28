b15c363e8078f6f20f83c2d8a9d0f70dd16c0b60
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-08-28 18:57:18 -0300

    feat: Created unit test for expense registration

---
+++ tests/UseCases.Testes/Expenses/Register/RegisterExpenseUseCaseTests.cs
@@ -0,0 +1,59 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Domain.Entities;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace UseCases.Testes.Expenses.Register;
+public class RegisterExpenseUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        var useCase = CreateUseCase(loggedUser);
+
+        //Act
+        var result = await useCase.Execute(request);
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Title.Should().Be(request.Title);
+    }
+
+    [Fact]
+    public async Task Error_Title_Empty()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var useCase = CreateUseCase(loggedUser);
+
+        //Act
+        var act = async () => await useCase.Execute(request);
+        
+        //Assert
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(ex => ex.GetErrorMessages().Count == 1 && ex.GetErrorMessages().Contains(ResourceErrorMessages.REQUIRED_TITLE));
+    }
+
+    private RegisterExpanseUseCase CreateUseCase(User user)
+    {
+        var repository = ExpensesWriteOnlyRepositoryBuilder.Build();
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new RegisterExpanseUseCase(repository, unitOfWork, mapper, loggedUser);
+    }
+}
