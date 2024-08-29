670c2bb15232aaf66f5971cd95da04b0d2d5f720
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-08-28 21:08:52 -0300

    feat: integration test generating an access token

--- tests/WebApi.Tests/CustomWebApplicationFactory.cs
+++ tests/WebApi.Tests/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +13,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     private CashFlow.Domain.Entities.User _user;
     private string _password;
+    private string _token;
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -29,14 +31,18 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                 var scope = services.BuildServiceProvider().CreateScope();
                 var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
                 var passwordEncrypter = scope.ServiceProvider.GetRequiredService<IPasswordEncrypter>();
+                var tokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
 
                 StartDatabase(dbContext, passwordEncrypter);
+
+                _token = tokenGenerator.Generate(_user);
             });
     }
 
     public string GetEmail() => _user.Email;
     public string GetName() => _user.Name;
     public string GetPassword() => _password;
+    public string GetToken() => _token;
 
     private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncrypter passwordEncrypter)
     {
---
+++ tests/WebApi.Tests/Expenses/Register/RegisterExpenseTests.cs
@@ -0,0 +1,72 @@
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Tests.InlineData;
+
+namespace WebApi.Tests.Expenses.Register;
+public class RegisterExpenseTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string METHOD = "api/Expenses";
+
+    private readonly HttpClient _httpClient;
+    private readonly string _token;
+
+    public RegisterExpenseTests(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+        _token = webApplicationFactory.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+        //Act
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+        
+        response.RootElement.GetProperty("title").GetString().Should().Be(request.Title);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Title_Empty(string cultureInfo)
+    {
+        //Arrange
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
+
+        //Act
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var expectedLanguage = ResourceErrorMessages.ResourceManager.GetString("REQUIRED_TITLE", new CultureInfo(cultureInfo));
+
+        var body = await result.Content.ReadAsStreamAsync();
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedLanguage));
+    }
+}
