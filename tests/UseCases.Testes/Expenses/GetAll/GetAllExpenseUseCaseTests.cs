e7792f75c18e3fc1eb1713d1f842596e0ccf1230
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-08-29 19:56:10 -0300

    feat: unit test to get all expenses

---
+++ tests/UseCases.Testes/Expenses/GetAll/GetAllExpenseUseCaseTests.cs
@@ -0,0 +1,41 @@
+﻿using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Testes.Expenses.GetAll;
+public class GetAllExpenseUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+        var expenses = ExpenseBuilder.Collection(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expenses);
+
+        //Act
+        var result = await useCase.Execute();
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Expenses.Should().NotBeNullOrEmpty().And.AllSatisfy(expense =>
+        {
+            expense.Id.Should().BeGreaterThan(0);
+            expense.Title.Should().NotBeNullOrEmpty();
+            expense.Amount.Should().BeGreaterThan(0);
+        });
+    }
+
+    private GetAllExpensesUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpenseReadOnlyRepositoryBuilder().GetAll(user, expenses).Build();
+        var autoMapper = MapperBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        return new GetAllExpensesUseCase(repository, autoMapper, loggedUser);
+    }
+}
