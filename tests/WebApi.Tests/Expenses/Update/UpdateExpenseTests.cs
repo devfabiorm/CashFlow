d2d7b40f777dc73bff4015ef2431bab45453e7e1
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-05 00:01:44 -0300

    feat: integration test for update an expense flow

---
+++ tests/WebApi.Tests/Expenses/Update/UpdateExpenseTests.cs
@@ -0,0 +1,85 @@
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Tests.InlineData;
+
+namespace WebApi.Tests.Expenses.Update;
+public class UpdateExpenseTests : CashFlowClassFixture
+{
+    private const string METHOD = "api/Expenses";
+
+    private readonly string _token;
+    private readonly long _expenseId;
+
+
+    public UpdateExpenseTests(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+        _expenseId = webApplicationFactory.Expense.GetId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var request = RequestExpenseJsonBuilder.Build();
+
+        //Act
+        var result = await DoPut(requestUri: $"{METHOD}/{_expenseId}", request: request, token: _token);
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Title_Empty(string culture)
+    {
+        //Arrange
+        var request = RequestExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        //Act
+        var result = await DoPut(requestUri: $"{METHOD}/{_expenseId}", request, token: _token, culture: culture);
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("REQUIRED_TITLE", new CultureInfo(culture));
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Expense_Not_Found(string culture)
+    {
+        //Arrange
+        var request = RequestExpenseJsonBuilder.Build();
+
+        //Act
+        var result = await DoPut(requestUri: $"{METHOD}/1000", request, token: _token, culture: culture);
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("EXPENSE_NOT_FOUND", new CultureInfo(culture));
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+}
