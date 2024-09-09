840eac7829b40c8989a590e192df8a4372d83845
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-09 20:44:27 -0300

    feat: created endpoint and use case to retrieve the logged user

---
+++ src/CashFlow.Application/UseCases/Users/Profile/GetUserProfileUseCase.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Services.LoggedUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Application.UseCases.Users.Profile;
+public class GetUserProfileUseCase : IGetUserProfileUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IMapper _mapper;
+
+    public GetUserProfileUseCase(ILoggedUser loggedUser, IMapper mapper)
+    {
+        _loggedUser = loggedUser;
+        _mapper = mapper;
+    }
+
+    public async Task<ResponseUserProfileJson> Execute()
+    {
+        var user = await _loggedUser.Get();
+
+        return _mapper.Map<ResponseUserProfileJson>(user);
+    }
+}
