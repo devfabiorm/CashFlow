5416d9c5a7e0d3d63bc0aef06cc51b44d8968f8c
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-09 18:40:52 -0300

    feat: unit and integration tests for expenses report generation

---
+++ tests/UseCases.Testes/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCaseTests.cs
@@ -0,0 +1,52 @@
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Testes.Expenses.Reports.Excel;
+public class GenerateExpensesReportExcelUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+        var expenses = ExpenseBuilder.Collection(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expenses);
+
+        //Act
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        //Assert
+        result.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Success_Empty()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+
+        var useCase = CreateUseCase(loggedUser, []);
+
+        //Act
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        //Assert
+        result.Should().BeEmpty();
+    }
+
+    private GenerateExpensesReportExcelUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpensesReadOnlyRepositoryBuilder()
+            .FilterByMonth(user, expenses)
+            .Build();
+
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new GenerateExpensesReportExcelUseCase(repository, loggedUser);
+    }
+}
---
+++ tests/UseCases.Testes/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCaseTests.cs
@@ -0,0 +1,52 @@
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Testes.Expenses.Reports.Pdf;
+public class GenerateExpensesReportPdfUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+        var expenses = ExpenseBuilder.Collection(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expenses);
+
+        //Act
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        //Assert
+        result.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Success_Empty()
+    {
+        //Arrange
+        var loggedUser = UserBuilder.Build();
+
+        var useCase = CreateUseCase(loggedUser, []);
+
+        //Act
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        //Assert
+        result.Should().BeEmpty();
+    }
+
+    private GenerateExpensesReportPdfUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpensesReadOnlyRepositoryBuilder()
+            .FilterByMonth(user, expenses)
+            .Build();
+
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new GenerateExpensesReportPdfUseCase(repository, loggedUser);
+    }
+}
