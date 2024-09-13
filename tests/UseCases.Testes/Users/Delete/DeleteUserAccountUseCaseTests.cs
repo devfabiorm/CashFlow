e29376ae884b8fed08d431aef7e474bf0a6f6016
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-09-12 23:42:22 -0300

    Test: unit and integration test for account deletion

---
+++ tests/UseCases.Testes/Users/Delete/DeleteUserAccountUseCaseTests.cs
@@ -0,0 +1,33 @@
+﻿using CashFlow.Application.UseCases.Users.Delete;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Testes.Users.Delete;
+public class DeleteUserAccountUseCaseTests
+{
+    public async Task Success()
+    {
+        //Arrange
+        var user = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        //Act
+        var act = async () => await useCase.Execute();
+
+        //Assert
+        await act.Should().NotThrowAsync();
+    }
+
+    private DeleteUserAccountUseCase CreateUseCase(User user)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        return new DeleteUserAccountUseCase(loggedUser, repository, unitOfWork);
+    }
+}
---
+++ tests/WebApi.Tests/Users/Delete/DeleteUserAccountTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System.Net;
+
+namespace WebApi.Tests.Users.Delete;
+public class DeleteUserAccountTests : CashFlowClassFixture
+{
+    private const string METHOD = "api/Users";
+
+    private readonly string _token;
+
+    public DeleteUserAccountTests(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var response = await DoDelete(requestUri: METHOD, token: _token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+}
